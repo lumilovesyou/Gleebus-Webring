1c72256f0ad0f2d8e20f207eab508d178638729d
lumilovesyou <felis.araneae@gmail.com>
2026-07-14 18:28:34 -0500

    Working Code

--- webring.js
+++ webring.js
@@ -1,1 +1,17 @@
-console.log(window.location.hostname)
+let webringSites;
+let positionInList;
+
+async function loadWebring() {
+    webringSites = (await (await fetch("https://raw.githubusercontent.com/lumilovesyou/Gleebus-Webring/refs/heads/main/webring.json")).json())["sites"];
+    positionInList = webringSites.indexOf(window.location.hostname);
+}
+
+function gleebusOpen(offset, newTab) {
+    window.open(`https://${getWrapped(webringSites, positionInList + offset)}/`, newTab ? "_blank" : "_self");
+}
+
+function getWrapped(array, index) {
+  return array[((index % array.length) + array.length) % array.length];
+}
+
+loadWebring();
